f2a3c8d226bbb817b579f0bfdd01df183e2f3d1b
Halil İbrahim ceylan <hibrahim.ceylan@std.hku.edu.tr>
2020-04-21 21:59:55 +0300

    HTTP SERVER

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,4 @@
 cp -rf /setup/data/* /data
 chown -R minecraft:minecraft /data
+( cd /setup/http && python3 -m http.server 25566 >/dev/null & )
 /start
--- setup/download-mods.sh
+++ setup/download-mods.sh
@@ -4,3 +4,5 @@ while IFS= read -r line
 do
     wget -Pdata/mods/ $line
 done <"$file"
+
+zip http/mods.zip data/mods/*.jar
