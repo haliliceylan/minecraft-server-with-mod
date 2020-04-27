1078e778704857e9add5cc0b658c8a2515a129bc
Halil İbrahim ceylan <hibrahim.ceylan@std.hku.edu.tr>
2020-04-27 19:53:04 +0300

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,5 @@
 test -f "/first-start" && (rm -rf /data/mods/*.jar; cp -rf /setup/data/* /data; rm -rf /first-start)
-zip -j /setup/http/mods.zip /data/mods/*.jar
+test -f "/setup/http/mods.zip" && rm -rf /setup/http/mods.zip
 cp -rf /data/mods /setup/http/
 chown -R minecraft:minecraft /data
 ( while true;do nohup http-server /setup/http --port 25566;done &>/dev/null & )
