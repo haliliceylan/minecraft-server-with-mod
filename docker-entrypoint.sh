de92a954660511b4b1b793e67ad90cc5f3487d5d
Halil İbrahim ceylan <hibrahim.ceylan@std.hku.edu.tr>
2020-04-28 11:11:40 +0300

    reset each start http server files

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,6 @@
 test -f "/first-start" && (rm -rf /data/mods/*.jar; cp -rf /setup/data/* /data; rm -rf /first-start)
 test -f "/setup/http/mods.zip" && rm -rf /setup/http/mods.zip
+test -d "/setup/http/" && rm -rf /setup/http/
 cp -rf /data/mods /setup/http/
 chown -R minecraft:minecraft /data
 ( while true;do nohup http-server /setup/http --port 25566;done &>/dev/null & )
